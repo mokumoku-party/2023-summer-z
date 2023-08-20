85ca88f20fcf94227f6b78c7b7b5959baa890e8e
dicenull <dicenull.dev@gmail.com>
2023-08-20 14:33:10 +0900

    🎨 描画に関する内容を基底クラスにまとめた

--- public/js/particles.js
+++ public/js/particles.js
@@ -2,6 +2,8 @@ class AbstractParticle {
   position;
   velocity;
   acceleration;
+  radius;
+  color;
 
   constructor(pos) {
     this.position = pos;
@@ -12,30 +14,25 @@ class AbstractParticle {
     this.position.add(p5.Vector.mult(this.velocity, deltaTime * .05));
   }
 
-  draw() {}
-}
-
-class RasingParticle extends AbstractParticle {
-  #color;
-
-  constructor(pos, color) {
-    super(pos);
-    this.velocity = createVector(0, random(-29, -20));
-    this.acceleration = createVector(0, .7);
-    this.#color = color;
-  }
-
   draw() {
-    strokeWeight(6);
+    strokeWeight(this.radius);
 
-    stroke(this.#color);
+    stroke(this.color);
     point(this.position.x, this.position.y);
   }
 }
 
+class RasingParticle extends AbstractParticle {
+  constructor(pos, color) {
+    super(pos);
+    this.velocity = createVector(0, random(-29, -20));
+    this.acceleration = createVector(0, .7);
+    this.color = color;
+    this.radius = 6;
+  }
+}
+
 class ExplodeParticle extends AbstractParticle {
-  #color;
-  #r;
   #lifespan;
   #velocityDist;
 
@@ -51,8 +48,8 @@ class ExplodeParticle extends AbstractParticle {
     this.velocity = v0;
     this.acceleration = createVector(0, .1);
 
-    this.#color = color;
-    this.#r = r;
+    this.color = color;
+    this.radius = r;
     this.#lifespan = initLife;
 
     this.#velocityDist = v;
@@ -64,17 +61,10 @@ class ExplodeParticle extends AbstractParticle {
     this.velocity.mult(this.#velocityDist);
     this.#lifespan -= 6;
 
-    this.#color.setAlpha(this.#lifespan);
+    this.color.setAlpha(this.#lifespan);
   }
 
   get done() {
     return this.#lifespan < 0;
-  }
-
-  draw() {
-    strokeWeight(this.#r);
-
-    stroke(this.#color);
-    point(this.position.x, this.position.y);
   }
 }
