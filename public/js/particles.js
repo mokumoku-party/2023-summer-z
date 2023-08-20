9cd53bf0f7874a65912839c33fbe9e9d8b060a1d
dicenull <dicenull.dev@gmail.com>
2023-08-20 14:29:56 +0900

    👍 花火のパラメータを追加

--- public/js/particles.js
+++ public/js/particles.js
@@ -36,23 +36,32 @@ class RasingParticle extends AbstractParticle {
 class ExplodeParticle extends AbstractParticle {
   #color;
   #r;
-  #initLife = 350;
-  #lifespan = this.#initLife;
+  #lifespan;
+  #velocityDist;
 
-  constructor(pos, color, r) {
+  constructor(
+    pos,
+    color,
+    r,
+    v = 0.95,
+    initLife = 350,
+    v0 = p5.Vector.random2D().mult(random(1, 15)),
+  ) {
     super(pos);
-    this.velocity = p5.Vector.random2D(); // ランダムにベクトルを定義
-    this.velocity.mult(random(1, 15));
+    this.velocity = v0;
     this.acceleration = createVector(0, .1);
 
     this.#color = color;
     this.#r = r;
+    this.#lifespan = initLife;
+
+    this.#velocityDist = v;
   }
 
   update() {
     super.update();
 
-    this.velocity.mult(0.95);
+    this.velocity.mult(this.#velocityDist);
     this.#lifespan -= 6;
 
     this.#color.setAlpha(this.#lifespan);
