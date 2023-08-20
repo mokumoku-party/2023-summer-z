bb1628f6448a6c20482fad86f70993ce58ad3b03
Ryo Hodota <ryo07011999@yahoo.co.jp>
2023-08-20 19:49:34 +0900

    花火発射する画面のデザインを作った

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -2,7 +2,9 @@ var fireworks = [];
 var gravity;
 
 function setup() {
-  createCanvas(displayWidth, displayHeight); // canvasを作成
+  const result = document.getElementById("canvas");
+  const canvas = createCanvas(windowWidth, windowHeight * 4 /5); // canvasを作成
+  canvas.parent(result);
   colorMode(HSB); //花火を出す色の指定の仕方
   gravity = createVector(0, 0.7);
   stroke(255); // 線の色を設定
@@ -10,6 +12,10 @@ function setup() {
   background(0); // 背景を黒く指定
 
   frameRate(120);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight * 4 /5);
 }
 
 function draw() {
